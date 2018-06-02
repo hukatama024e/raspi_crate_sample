7ba893dd9f30d38edc23de91b2359a53214c9a59
hukatama024e <physaliaphysalisdev@gmail.com>
2018-06-03 08:09:36 +0900

    Add spi sample for rustpi_io

--- rustpi_io_sample/src/main.rs
+++ rustpi_io_sample/src/main.rs
@@ -1,8 +1,10 @@
 extern crate rustpi_io;
 
+use std::io::{Read, Write};
 use std::thread;
 use std::time::Duration;
 use rustpi_io::gpio::{GPIO, GPIOData, GPIOMode};
+use rustpi_io::serial::{SerialPi, Device, Speed, SpiMode, ComMode};
 
 // GPIO BCM pin number
 const GPIO_PIN_NUM : u8 = 24;
@@ -22,5 +24,21 @@ fn main() {
         gpio.set( GPIOData::Low ).expect( "Failed GPIO.set()" );
         thread::sleep( Duration::from_secs( 1 ) );
         blinking_cnt = blinking_cnt + 1;
+    }
+
+    // SPI setting
+    let mut spi = SerialPi::new( Device::CE0, Speed::Mhz62_5, SpiMode::Mode0, ComMode::FullDuplex )
+                    .expect( "Failed SerialPi::new()" );
+
+    // Write SPI data
+    let mut write_data = vec![0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0];
+    spi.write( &mut write_data ).expect( "Failed SerialPi::write" );
+
+    // Read SPI data
+    let mut read_data = [0; 8];
+    let read_size = spi.read( &mut read_data ).expect( "Failed SerialPi::read" );
+
+    for i in 0..read_size {
+        println!( "read_data[{}] = 0x{:X}", i, read_data[i] );
     }
 }
