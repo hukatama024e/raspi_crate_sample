bb36a5f3bedb7f6f6a9ef1ceeba1f601408156da
hukatama024e <physaliaphysalisdev@gmail.com>
2018-06-11 03:46:12 +0900

    Add rppal sample for SPI

--- rppal_sample/src/main.rs
+++ rppal_sample/src/main.rs
@@ -2,7 +2,9 @@ extern crate rppal;
 
 use std::thread;
 use std::time::Duration;
-use rppal::gpio::{Gpio, Mode, Level};
+use rppal::{gpio, spi};
+use rppal::gpio::{Gpio, Level};
+use rppal::spi::{Spi, Bus, SlaveSelect};
 
 // GPIO BCM pin number
 const GPIO_PIN_NUM : u8 = 24;
@@ -15,7 +17,7 @@ fn main() {
     let mut blinking_cnt = 0;
 
     // Change GPIO mode to output
-    gpio.set_mode( GPIO_PIN_NUM, Mode::Output );
+    gpio.set_mode( GPIO_PIN_NUM, gpio::Mode::Output );
 
     // Toggle GPIO pinfor blinking an LED
     while blinking_cnt < BLINK_CNT {
@@ -28,5 +30,18 @@ fn main() {
     }
 
     // Return GPIO mode to input
-    gpio.set_mode( GPIO_PIN_NUM, Mode::Input );
+    gpio.set_mode( GPIO_PIN_NUM, gpio::Mode::Input );
+
+    // SPI setting
+    let spi = Spi::new( Bus::Spi0, SlaveSelect::Ss0, 6_2500_000, spi::Mode::Mode0 )
+                    .expect( "Failed Spi::new" );
+
+    // write and read SPI data
+    let write_data = vec![0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC, 0xDE, 0xF0];
+    let mut read_data = [0; 8];
+    let read_size = spi.transfer( &mut read_data, &write_data ).expect( "Failed Spi::transfer" );
+
+    for i in 0..read_size {
+        println!( "read_data[{}] = 0x{:X}", i, read_data[i] );
+    }
 }
